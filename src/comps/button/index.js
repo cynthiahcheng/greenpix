95d5250c3c1fd01e2aed65fe21d630b66dd4b459
= <=>
2021-03-04 18:52:50 -0800

    installed framer-motion, built button and input

--- src/comps/button/index.js
+++ src/comps/button/index.js
@@ -1,21 +1,47 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import { motion } from 'framer-motion';
 
 const Container = styled.div`
-
+display:flex;
+justify-content:${props => props.justify ? props.justify : "center"};
 `;
 
 const ButtonBox = styled.button`
+padding:${props => props.padding ? props.padding : "15px 100px"};
+align-items:center;
+cursor:${props => props.cursor ? props.cursor : "pointer"};
+border-radius:${props => props.borderRadius ? props.borderRadius : "12px"};
+font-weight:${props => props.fontWeight ? props.fontWeight : "400"};
+font-size:${props => props.fontSize ? props.fontSize : "16px"};
+background-color:${props => props.bgColor ? props.bgColor : "#FFF"};
+border:${props => props.border ? props.border : "1.75px solid #99C789"};
+margin:${props => props.margin ? props.margin : "8px"};
+&:focus {outline:none;}
 `;
 
-const Button = ({}) => {
-    return <Container>
-        <ButtonBox>I am the button comp</ButtonBox>
+const Button = ({ text, fontWeight, fontSize, bgColor, hoverColor, padding, border, borderRadius, margin, justify, cursor, onClick }) => {
+    return <Container justify={justify}>
+        <motion.div whileTap={{ scale: 0.96 }}>
+            <ButtonBox 
+                fontWeight={fontWeight}
+                fontSize={fontSize}
+                bgColor={bgColor}
+                hoverColor={hoverColor}
+                padding={padding}
+                border={border}
+                borderRadius={borderRadius}
+                margin={margin}
+                cursor={cursor}
+                onClick={onClick}
+            >{text}</ButtonBox>
+        </motion.div>  
     </Container>
 }
 
 Button.defaultProps = {
-
+text:"I am a button",
+onClick:()=>{}
 };
 
 export default Button;
--- src/comps/input/index.js
+++ src/comps/input/index.js
@@ -5,9 +5,27 @@ const Container = styled.div`
 
 `;
 
-const Input = ({}) => {
-    return <Container>
+const InputBox = styled.input`
+padding:15px;
+min-width:${props => props.minWidth ? props.minWidth : "280px"};
+font-size:${props => props.fontSize ? props.fontSize : "16px"};
+border-radius:${props => props.borderRadius ? props.borderRadius : "10px"};
+    &:focus {
+        outline:0;
+    }
+border:${props => props.border ? props.border : "1.75px solid #99C789"};
+`;
 
+const Input = ({minWidth, fontSize, borderRadius, border}) => {
+    return <Container>
+        <InputBox type="text" placeholder="Email" 
+            minWidth={minWidth}
+            fontSize={fontSize}
+            borderRadius={borderRadius}
+            border={border}
+        >
+
+        </InputBox>
     </Container>
 }
 
