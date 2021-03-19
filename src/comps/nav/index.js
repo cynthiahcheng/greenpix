b2bdabca10de7368670781637f5901afbf048723
= <=>
2021-03-19 14:44:37 -0700

    Finished editing Nav

--- src/comps/nav/index.js
+++ src/comps/nav/index.js
@@ -8,28 +8,45 @@ import woman from '../bioCard/woman.jpg';
 const Container = styled.footer`
   display: flex;
   align-items: center;
-  justify-content: center;
-  justify-content: space-evenly;
   background-color: #BDEAC0;
-  padding: 10px;
+`;
+
+const NavBox = styled.div`
+width: 375px;
+height: 62px;
+display: flex;
+align-items: center;
+justify-content: space-around;
 `;
 
 
 const Img = styled.img`
 padding: 10px;
+width: 26px;
+height: 26px;
+cursor: pointer;
 `;
 
 const Img2 = styled.img`
 padding: 10px;
 border-radius: 50%;
+cursor: pointer;
 `;
 
-const Nav = ({}) => {
-    return  <Container> 
- <Img src={Home}alt="home" width="33.5px" height="33.5px"/>
+
+const Nav = ({ onClick }) => {
+
+  const handleClick = e => {
+    
+  }
+
+    return  <Container>
+      <NavBox>
+ <Img src={Home}alt="home" width="33.5px" height="33.5px" onClick={handleClick}/>
  <Img src={Search}alt="search" width="33.5px" height="33.5px"/>
  <Img src={Add}alt="add" width="33.5px" height="33.5px"/>
  <Img2 src={woman}alt="pic" width="33.5px" height="33.5px"/>
+ </NavBox>
     </Container>
 }
 
